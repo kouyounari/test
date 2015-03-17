545e535cd0b2d278cc79514814b9f47ed90c9ff0
kouyounari <kouyou.nari@gmail.com>
2015-03-17 20:09:26 +0900

    Spring boot1.2移行

--- hajiboot-rest/src/main/java/com/example/App.java
+++ hajiboot-rest/src/main/java/com/example/App.java
@@ -1,15 +1,13 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class App 
 {
     
