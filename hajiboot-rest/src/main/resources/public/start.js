42d3bd3db3c9f25a604a03eb004ca69283564abf
kouyounari <kouyou.nari@gmail.com>
2015-03-22 00:26:49 +0900

    Angular1.4移行

--- hajiboot-rest/src/main/resources/public/start.js
+++ hajiboot-rest/src/main/resources/public/start.js
@@ -1,11 +1,10 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-function Hello($scope, $http) {
-    $http.get('http://localhost:8080/api/customer').success(function(data) {
+angular.module('myApp', []).controller('CustomerCtrl', function($scope, $http) {
+        $http.get('/api/customer').success(function(data) {
             $scope.customers = data;
         });
-}
-
+});
